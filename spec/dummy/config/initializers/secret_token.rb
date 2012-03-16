8b290ce01c9dd50ef242645fd4f57db24b3a03ec
Winston Teo <pair+winston@newcontext.com>
2012-03-16 11:33:17 +0800

    Dummy rails app for testing. From "rails g plugin new",and customizations made to config/application.rb.

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '4b6ed3d1cd1d90efd618d464ec46edfd9a5ccc2a152df4294cb8da0251ec322d33ce972dd91856ac80454f546c24703b59483d804c7ce7a3b9d279e9ba467c9a'
